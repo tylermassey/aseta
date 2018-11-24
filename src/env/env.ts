205295a240b963c1979292a0e303399cded45a2b
Tyler Massey <tylermassey6@gmail.com>
2018-11-23 23:31:19 -0600

    Add env file

---
+++ src/env/env.ts
@@ -0,0 +1,12 @@
+import { Config } from './Config';
+
+export default {
+    firebase: {
+        apiKey: 'AIzaSyATl0uwiGZOs-3jpU2FYaZPtIg6QcBT8xg',
+        authDomain: 'aseta-testing.firebaseapp.com',
+        databaseURL: 'https://aseta-testing.firebaseio.com',
+        projectId: 'aseta-testing',
+        storageBucket: 'aseta-testing.appspot.com',
+        messagingSenderId: '201465564812',
+    },
+} as Config;
